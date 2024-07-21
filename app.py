7b3b0c86c7dd6e2f0bb1e36fdba4a868a2edc7ce
John <cienfuegosjohn@gmail.com>
2024-07-21 08:44:22 -0400

    working

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, request, send_from_directory, jsonify
-import os
-import youtube_dl
+from flask import Flask, render_template, request, jsonify
+import yt_dlp as youtube_dl
 import ssl
 import certifi
+import os
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 def download_youtube_video(url, output_path=''):
     # Ensure output_path ends with a slash
@@ -23,27 +25,25 @@ def download_youtube_video(url, output_path=''):
             'verify': certifi.where()
         }
     }
-    video_title = None
     try:
         ssl._create_default_https_context = ssl._create_unverified_context
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(url, download=True)
-            video_title = ydl.prepare_filename(info_dict)
-        return video_title
+            ydl.download([url])
+        return f"Downloaded: {url}"
     except Exception as e:
         return str(e)
+
 
 @app.route('/download', methods=['POST'])
 def download_video():
     url = request.form['url']
-    downloads_path = os.path.expanduser('~/Downloads/')  # Ensure this path is accessible on the server
-    video_path = download_youtube_video(url, output_path=downloads_path)
-    if video_path and os.path.exists(video_path):
-        directory = os.path.dirname(video_path)
-        filename = os.path.basename(video_path)
-        return send_from_directory(directory, filename, as_attachment=True)
+    downloads_path = os.path.expanduser('~/Downloads/')
+    result = download_youtube_video(url, output_path=downloads_path)
+    if "Downloaded" in result:
+        return jsonify({'message': result})
     else:
-        return jsonify({'error': 'Failed to download the video.'}), 500
+        return jsonify({'error': result}), 500
+
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)  # Make sure to run on all interfaces
+    app.run(debug=True)
